c34cb59afbdada7a1999cf810914fb52eefc4ce1
Abirami.R <77112799+Abi-05@users.noreply.github.com>
2021-01-29 18:35:15 +0530

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,9 @@
-#Python program to generate Fibonacci series until 'n' value
-n = int(input("Enter the value of 'n': "))
-a = 0
-b = 1
-sum = 0
-count = 1
-print("Fibonacci Series: ", end = " ")
-while(count <= n):
-  print(sum, end = " ")
+nterms = int(input("How many terms? :"))
+n1, n2 = 0, 1
+count = 0
+while count < nterms:
+  print(n1)
+  nth = n1 + n2
+  n1 = n2
+  n2 = nth
   count += 1
-  a = b
-  b = sum
-  sum = a + b
